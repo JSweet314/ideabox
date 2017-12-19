466e4578e7e5172ac634a0d8d1c41214ec464676
Jon Sweet <jonathan.h.sweet@gmail.com>
2017-12-18 18:13:10 -0700

    add ability to update local storage when idea is deleted or quality is changed

--- scripts/ideabox.js
+++ scripts/ideabox.js
@@ -1,49 +1,20 @@
 var $userTitle = $('#idea-title');
 var $userBody = $('#idea-body');
 var $userIdeas = $('main');
-var ideaNumber = 0;
 
-function Idea(title, body) {
-  this.id = Date();
-  this.title = title;
-  this.body = body;
-  this.quality = 'swill';
-}
-
-Idea.prototype.prependIdea = function() {
-  $userIdeas.prepend(
-  `<div>
-      <h2>${this.title}</h2>
-      <input class="delete-button" type="image" src="images/delete.svg" alt="delete">
-      <p>${this.body}</p>
-      <input class="upvote-button" type="image" src="images/upvote.svg" alt="up-vote">
-      <input class="downvote-button" type="image" src="images/downvote.svg" alt="down-vote"> 
-      <span class="quality">quality: ${this.quality}</span>
-      <hr />
-      </div>`);
-};
-
-function changeQualityUp() {
-  if ($(this).siblings('.quality').text() === 'quality: swill') {
-    $(this).siblings('.quality').text('quality: plausible');
-  } else if ($(this).siblings('.quality').text() === 'quality: plausible') {
-    $(this).siblings('.quality').text('quality: genius');
-  } 
-};
-
-function changeQualityDown() {
-  if ($(this).siblings('.quality').text() === 'quality: genius') {
-    $(this).siblings('.quality').text('quality: plausible');
-  } else if ($(this).siblings('.quality').text() === 'quality: plausible') {
-    $(this).siblings('.quality').text('quality: swill');
+window.onload = function(event){
+  for (i = 0; i < localStorage.length; i++){
+    var stringifiedIdea = localStorage.getItem(localStorage.key(i));
+    var parsedIdea = JSON.parse(stringifiedIdea);
+    prependIdea(parsedIdea);
   }
 };
 
 $('button').on('click', function(event){
   event.preventDefault();
   var idea = new Idea($userTitle.val(), $userBody.val());
-  console.log(idea);
-  idea.prependIdea();
+  localStorage.setItem(idea.id, JSON.stringify(idea));
+  prependIdea(idea);
   $userTitle.val('');
   $userBody.val('');
 });
@@ -53,5 +24,54 @@ $userIdeas.on('click', '.upvote-button', changeQualityUp);
 $userIdeas.on('click', '.downvote-button', changeQualityDown);
 
 $userIdeas.on('click', '.delete-button', function(){
+  var storageID = $(this).parent().data('id');
+  localStorage.removeItem(storageID);
   $(this).parent().remove();
 });
+
+function Idea(title, body) {
+  this.id = Date.now();
+  this.title = title;
+  this.body = body;
+  this.quality = 'swill';
+}
+
+function prependIdea(ideaObject) {
+  $userIdeas.prepend(
+    `<div>
+    <h2>${ideaObject.title}</h2>
+    <input class="delete-button" type="image" src="images/delete.svg" alt="delete">
+    <p>${ideaObject.body}</p>
+    <input class="upvote-button" type="image" src="images/upvote.svg" alt="up-vote">
+    <input class="downvote-button" type="image" src="images/downvote.svg" alt="down-vote"> 
+    <span class="quality">quality: ${ideaObject.quality}</span>
+    <hr />
+    </div>`);
+  $('div:first').data('id', ideaObject.id);
+};
+
+function changeQualityUp() {
+  var storageID = $(this).parent().data('id');
+  var parsedIdea = JSON.parse(localStorage.getItem(storageID));
+  if ($(this).siblings('.quality').text() === 'quality: swill') {
+    $(this).siblings('.quality').text('quality: plausible');
+    parsedIdea.quality = 'plausible';
+  } else if ($(this).siblings('.quality').text() === 'quality: plausible'){
+    $(this).siblings('.quality').text('quality: genius');
+    parsedIdea.quality = 'genius';
+  } 
+  localStorage.setItem(parsedIdea.id, JSON.stringify(parsedIdea));
+};
+
+function changeQualityDown() {
+  var storageID = $(this).parent().data('id');
+  var parsedIdea = JSON.parse(localStorage.getItem(storageID));
+  if ($(this).siblings('.quality').text() === 'quality: genius') {
+    $(this).siblings('.quality').text('quality: plausible');
+    parsedIdea.quality = 'plausible';
+  } else if ($(this).siblings('.quality').text() === 'quality: plausible') {
+    $(this).siblings('.quality').text('quality: swill');
+    parsedIdea.quality = 'swill';
+  }
+  localStorage.setItem(parsedIdea.id, JSON.stringify(parsedIdea));
+};
